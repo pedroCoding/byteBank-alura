6e9fde050513dceb5daf4047629f4baa1e76f3bd
Pedro Lucas <107092245+pedroCoding@users.noreply.github.com>
2023-04-08 18:10:01 -0300

    Adicionando arquivos via UPLOAD

---
+++ bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,17 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+
+	}
+
+}
---
+++ bytebank/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PEDRO
+ *
+ */
+module bytebank {
+}
