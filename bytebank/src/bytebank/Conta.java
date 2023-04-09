845a3290ada849ae103f5db5857dad68dba509d0
PEDRO <PEDRO@DESKTOP-SVUEHIN>
2023-04-09 15:28:47 -0300

    Cria TestaMetodo + Metodo na classe Conta (Deposita)

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -7,5 +7,13 @@ public class Conta {
 	int numero;
 	String titular;
 	
+	//MÉTODO DEPOSITA (Não retorna nada - VOID)
+	void deposita(double valor) {
+		this.saldo = this.saldo + valor;
+		System.out.println("Deposito realizado com sucesso. O valor depositado é de: R$"+valor);
+	}
+	
+	//MÉTODO SACAR
+	
 
 }
---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,14 @@
+package bytebank;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conta conta = new Conta();
+		conta.saldo = 200;
+		conta.deposita(100);
+
+	}
+
+}
